d67ea444f499d3899e324868c0367dae2d0ffb28
AmandaLSmithlin <AmandaLSmithlin@gmail.com>
2021-12-30 10:43:13 -0500

    js-basics

---
+++ js-basics/javaScript_practice/ReadMe2.txt/ReadMe2.txt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReadMe2.txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
